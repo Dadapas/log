65974c68a4b242ae79af0277912227b049975708
TOVOHERY <tovoherypascal@gmail.com>
2022-06-11 18:59:27 +0300

    Make a test for all

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -13,56 +13,98 @@ namespace Dadapas\LogTests;
 
 use PHPUnit\Framework\TestCase;
 use Dadapas\Log\{LogException, Log as Logger};
+use Dadapas\Log\FileSystemAdapter;
+use Dadapas\Log\PHPMailerAdapter;
 use Psr\Log\InvalidArgumentException;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception as MailerException;
+use Exception;
 
 final class LogTest extends TestCase
 {
-	protected $logger;
+	protected $loggerFile;
+
+	protected $loggerEmail;
 
 	/**
 	 * @before
 	*/
 	protected function runFirst()
 	{
-		$this->logger = new Logger();
-		try {
-			$this->logger->setPath(dirname(__DIR__, 1) . '/logs');
-		} catch (LogException $e) {
+		$this->loggerFile = new Logger();
+		$this->loggerEmail = new Logger();
 
-			$this->assertSame('File is not writable', $e->getMessage());
-		}
+		$adapter = new \League\Flysystem\Local\LocalFilesystemAdapter(
+		    dirname(__DIR__).'/logs'
+		);
+		$filesystem = new \League\Flysystem\Filesystem($adapter);
+
+		$filesystemAdapter = new FileSystemAdapter($filesystem);
+
+		// Local file adapter
+		$this->loggerFile->setAdapter($filesystemAdapter);
+
+		// Set up a php mailer logger
+		//
+		$mail = new PHPMailer(true);
+
+		//Server settings
+	    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+	    $mail->isSMTP();                                            //Send using SMTP
+	    $mail->Host = 'smtp.gmail.com';
+		$mail->Port = 465;
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = 'emai@gmail.com';
+
+		//Password to use for SMTP authentication
+		$mail->Password = 'password';
+
+		//Set who the message is to be sent from
+		//Note that with gmail you can only use your account address (same as `Username`)
+		//or predefined aliases that you have configured within your account.
+		//Do not use user-submitted addresses in here
+		$mail->setFrom('fromemail@gmail.com', 'First Last');
+
+		$mail->addAddress('toemaiil@email.com', 'Sencond last');
+
+	    $emailAdapter = new PHPMailerAdapter($mail);
+
+		// Email adapter
+		$this->loggerEmail->setAdapter($emailAdapter);
 	}
 
 	public function test_logfile()
 	{
-		$logger = $this->logger;
+		$logger = $this->loggerFile;
 
-		$ret = $logger->error('message has been thrownds', ['balfezge', 'lfezkgjezf']);
-		$this->assertSame(null, $ret);
-	}
-
-	public function test_using_trycatch()
-	{
-		$logger = $this->logger;
 		try {
-			throw new \InvalidArgumentException("not have enough space.");
-		} catch (\InvalidArgumentException $e) {
+		    throw new Exception("An exception has been thrown.");
+		} catch (Exception $e) {
 
-			//var_dump($e->getTrace());die;
-			$logger->error($e->getMessage(), $e->getTrace());
-			$this->assertInstanceOf(\InvalidArgumentException::class, $e);
+			$ret = $logger->error($e->getMessage(), $e->getTrace());
+			$this->assertSame(null, $ret);
 		}
 	}
 
-	public function test_invalid_level()
+	/*public function test_logemail()
 	{
-		$logger = $this->logger;
-		try {
-			$logger->log('invalid level', 'message for this invalid level' );
-		} catch (InvalidArgumentException $e) {
+		$logger = $this->loggerEmail;
 
-			$logger->error($e->getMessage(), $e->getTrace());
-			$this->assertInstanceOf(InvalidArgumentException::class, $e);
+		try {
+		    throw new Exception("An exception has been thrown.");
+		} catch (Exception $e) {
+
+			$ret = $logger->error($e->getMessage(), $e->getTrace());
+			$this->assertSame(null, $ret);
+		} catch (MailerException $e) {
+
+		    $this->assertSame("Message could not be sent. Mailer Error: {$mail->ErrorInfo}", $e->getMessage());
 		}
-	}
+	}*/
+
 }
