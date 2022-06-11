b6bdb4436f6107c60b6bd1d511c1336f93de0239
TOVOHERY <tovoherypascal@gmail.com>
2022-06-11 13:21:10 +0300

    custom exception.

---
+++ src/LogException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Dadapas\Log;
+
+/**
+ * This file is part of the dadapas/log library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) TOVOHERY Z. Pascal <tovoherypascal@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+class LogException extends \Exception {}
